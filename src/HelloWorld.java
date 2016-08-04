81021fb4f6f6effa054a251fdbb20574170472fa
Olivier Korach <olivier.korach@gmail.com>
2016-08-04 15:25:54 +0200

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,5 +4,9 @@ public class HelloWorld extends Object {
     System.out.println("Hello World !");
 
     }
-  
+    public void doSomething(String [] strings) {
+        for (int i = 0; i < strings.length; i--) { // Noncompliant;
+        String string = strings[i];  // ArrayIndexOutOfBoundsException when i reaches -1
+        //...
+  }
 }
