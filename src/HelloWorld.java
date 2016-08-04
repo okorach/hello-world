b402582cc91a8c865f8dbaaf5c32e3265192ebbd
Olivier Korach <olivier.korach@gmail.com>
2016-08-04 15:22:58 +0200

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,25 +2,7 @@ public class HelloWorld extends Object {
 
     public static void main(String[] args) {
     System.out.println("Hello World !");
-    System.out.println("Hello World !");
-    System.out.println("Hello World !");
-    System.out.println("Hello World !");
-    System.out.println("Hello World !");
-    
-    System.out.println("Hello World in PR !");
-    
-    System.out.println("Test PR analysis !");
-    System.out.println("Test PR analysis !");
-    System.out.println("Test PR analysis !");
-        System.out.println("Test PR analysis !");
-            System.out.println("Test PR analysis !");
-    System.out.println("Test PR analysis !");
-    }
 
-public void doSomething(String [] strings) {
-  for (int i = 0; i < strings.length; i--) { // Noncompliant;
-    String string = strings[i];  // ArrayIndexOutOfBoundsException when i reaches -1
-    //...
-  }
+    }
   
 }
