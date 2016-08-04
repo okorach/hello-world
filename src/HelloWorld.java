2565eeb7d9ff337bb7b2accc08d46bec25e6a5e0
Olivier Korach <olivier.korach@gmail.com>
2016-08-04 15:14:17 +0200

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -17,4 +17,10 @@ public class HelloWorld extends Object {
     System.out.println("Test PR analysis !");
     }
 
+public void doSomething(String [] strings) {
+  for (int i = 0; i < strings.length; i--) { // Noncompliant;
+    String string = strings[i];  // ArrayIndexOutOfBoundsException when i reaches -1
+    //...
+  }
+  
 }
