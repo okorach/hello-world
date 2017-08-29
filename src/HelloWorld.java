6b490d98dd3e11cf240922c35bc2047df275157a
Olivier Korach <olivier.korach@sonarsource.com>
2017-08-29 07:50:23 -0400

    Introducing blocker

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld extends Object {
 
     public static void main(String[] args) {
-    System.out.println("Hello World !");
+    System.out.println("Hello World 2!");
 
     }
     public void doSomething(String [] strings) {
