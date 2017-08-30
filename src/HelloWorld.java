94f112258a7b557e8ebfe854439eb2d931f0a4ad
Olivier Korach <olivier.korach@sonarsource.com>
2017-08-29 22:23:56 -0400

    Introducing blocker

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,10 +4,13 @@ public class HelloWorld extends Object {
     System.out.println("Hello World 2!");
 
     }
+    /*
     public void doSomething(String [] strings) {
+
         for (int i = 0; i < strings.length; i--) { // Noncompliant;
             String string = strings[i];  // ArrayIndexOutOfBoundsException when i reaches -1
         //...
         }
     }
+    */
 }
